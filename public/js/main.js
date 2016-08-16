8271e25f5e0990c45e22efaaa0102b46f29d8c97
Gabriel Antunes <printfgabriel@gmail.com>
2016-08-16 14:07:44 -0300

    Modernizr and Firebase bindings

---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyA_jMasg5G4S2FPIM60ObYZMwzayiAhOmU",
+    authDomain: "jogodaacentuacao.firebaseapp.com",
+    databaseURL: "https://jogodaacentuacao.firebaseio.com",
+    storageBucket: "jogodaacentuacao.appspot.com",
+};
+firebase.initializeApp(config);
